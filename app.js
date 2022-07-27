1ee43a25a0ab28e1eaa06ea273f58a0fa40abdee
Thiagohcrestani <30701875+Thiagohcrestani@users.noreply.github.com>
2022-07-26 23:39:07 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+
+mongoose.connect(process.env.MONGODB_URI,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }, err => {
+        if (err) throw err;
+        console.log('Connected to MongoDB!!!')
+    });
+
+    
+require('./api/models/endereco');
+require('./api/models/pessoa');
+
+
+const app = express();
+
+const enderecosRoutes = require('./api/routes/enderecos');
+const pessoasRoutes = require('./api/routes/pessoas');
+
+
+app.use('/enderecos', enderecosRoutes);
+app.use('/pessoas', pessoasRoutes);
+app.use(morgan('dev'));
+
+app.use('/api', (req, res, next) => {
+    res.status(200).json({
+        message: 'Hello word!'
+    })
+})
+
+app.use((req, res, next) => {
+    const error = new Error('Not Found')
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+});
+
+module.exports = app;
